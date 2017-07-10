ac8b4e90494fbb16788d03c93dbf3f82ae2747f6
João Pedro Leôncio <joopeeds@gmail.com>
2017-07-10 20:59:29 +1200

    updating

--- js/google_map.js
+++ js/google_map.js
@@ -5,13 +5,13 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(-36.850939, 174.764464);
+    var myLatlng = new google.maps.LatLng(-36.863288, 174.763016);
     // 39.399872
     // -8.224454
     
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 15,
+        zoom: 16,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
@@ -30,14 +30,13 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['50 Mayoral Dr'];
+    var addresses = ['50 Burton St'];
 
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
             var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
             new google.maps.Marker({
-                position: latlng,
+                position: myLatlng,
                 map: map,
                 icon: 'images/loc.png'
             });
